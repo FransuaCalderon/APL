b5ac1ff8a9ac61c442220c29602645d08ee42e6f
joseDG <josethp1@gmail.com>
2025-10-08 12:01:09 -0500

    restaurar el archivo cprog

--- AppAPL.Core/AppAPL.AccesoDatos/obj/Debug/net9.0/AppAPL.AccesoDatos.AssemblyInfo.cs
+++ AppAPL.Core/AppAPL.AccesoDatos/obj/Debug/net9.0/AppAPL.AccesoDatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppAPL.AccesoDatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c596b8b0559675663aef2e223d88bdeffab1d56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8533fe3883683d2259f8da25775ea0b715eab464")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppAPL.AccesoDatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppAPL.AccesoDatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AppAPL.Core/AppAPL.Dto/obj/Debug/net9.0/AppAPL.Dto.AssemblyInfo.cs
+++ AppAPL.Core/AppAPL.Dto/obj/Debug/net9.0/AppAPL.Dto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c596b8b0559675663aef2e223d88bdeffab1d56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8533fe3883683d2259f8da25775ea0b715eab464")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
