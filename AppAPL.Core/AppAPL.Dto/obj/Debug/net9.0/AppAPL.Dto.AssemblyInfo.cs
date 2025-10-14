b9ddf35d213e5fd546927662c4a7f1b486824333
Daniel <juandaniel34@hotmail.es>
2025-10-14 17:50:30 -0500

    antes del pull

--- AppAPL.Core/AppAPL.Dto/obj/Debug/net9.0/AppAPL.Dto.AssemblyInfo.cs
+++ AppAPL.Core/AppAPL.Dto/obj/Debug/net9.0/AppAPL.Dto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad1a0e61d80b14c7e9b2b3cf1884cc056a7edff4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792850c3972917afa79b6796bdc1400b7d1f9298")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AppAPL.Core/AppAPL.Negocio/obj/Debug/net9.0/AppAPL.Negocio.AssemblyInfo.cs
+++ AppAPL.Core/AppAPL.Negocio/obj/Debug/net9.0/AppAPL.Negocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppAPL.Negocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad1a0e61d80b14c7e9b2b3cf1884cc056a7edff4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792850c3972917afa79b6796bdc1400b7d1f9298")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppAPL.Negocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppAPL.Negocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
