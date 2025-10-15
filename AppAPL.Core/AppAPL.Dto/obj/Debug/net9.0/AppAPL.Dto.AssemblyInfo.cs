015323dbdc22bc75bb2f72f184f30da8fc42122b
Jean Calderón <jean.calderonv@ug.edu.ec>
2025-10-15 12:54:43 -0500

    Mejora de Tipo Catalogo y Opciones en configuracion ediatr y agregar modal y btones de paginación etc. Front

--- AppAPL.Core/AppAPL.Dto/obj/Debug/net9.0/AppAPL.Dto.AssemblyInfo.cs
+++ AppAPL.Core/AppAPL.Dto/obj/Debug/net9.0/AppAPL.Dto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd3b7e2137e6600ded0736afa9204f394b566b96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09f129c1122ce411f9ab3b5e91336ae5ec255519")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
