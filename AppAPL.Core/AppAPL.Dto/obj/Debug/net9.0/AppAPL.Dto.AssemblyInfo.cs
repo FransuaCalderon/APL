84e208efe927a2c6f1e8ced28332ca26478ec4a0
Jean Calderón <jean.calderonv@ug.edu.ec>
2025-10-16 09:34:57 -0500

    Modificación de diseño del Headers y de colores del datatables agregado de botones agregar y limpiar - Front

--- AppAPL.Core/AppAPL.Dto/obj/Debug/net9.0/AppAPL.Dto.AssemblyInfo.cs
+++ AppAPL.Core/AppAPL.Dto/obj/Debug/net9.0/AppAPL.Dto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09f129c1122ce411f9ab3b5e91336ae5ec255519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+015323dbdc22bc75bb2f72f184f30da8fc42122b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppAPL.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
