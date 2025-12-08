9ee95126696fee82610ba724d1e7fea538d957ed
joseDG <josethp1@gmail.com>
2025-12-08 15:06:16 -0500

    actualizacion del sp de los combos

---
+++ Oracle/Scripts_SP/Apl_Sp_Combos_Catalogo_Tipo.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE PROCEDURE APL_SP_COMBOS_CATALOGO_TIPO (
+    p_etiqueta IN VARCHAR2,
+    p_cursor   OUT SYS_REFCURSOR
+)
+AS
+BEGIN
+    OPEN p_cursor FOR
+        SELECT CASE 
+                   WHEN cp.idetiqueta = p_etiqueta THEN 'CATALOGOTIPO'
+                   WHEN c.idetiqueta = p_etiqueta THEN 'CATALOGO'
+               END AS origen_etiqueta,
+               cp.idcatalogotipo,
+               cp.nombre AS nombre_tipoCatalogo,
+               cp.idetiqueta AS idetiqueta_tipoCatalogo,
+               c.idcatalogo,
+               c.nombre AS nombre_Catalogo,
+               c.adicional AS adicional_Catalogo,
+               c.abreviatura AS abreviatura_Catalogo,
+               c.idetiqueta AS idetiqueta_Catalogo
+        FROM APL_TB_CATALOGOTIPO cp  
+        INNER JOIN APL_TB_CATALOGO c ON cp.idcatalogotipo = c.idcatalogotipo
+        WHERE cp.idetiqueta = p_etiqueta 
+           OR c.idetiqueta = p_etiqueta;
+        
+END APL_SP_COMBOS_CATALOGO_TIPO;
