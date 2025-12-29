063f7672544a119ffbbfea32286d020b1d078f77
Daniel <juandaniel34@hotmail.es>
2025-12-29 12:57:27 -0500

    Creacion test controllador para probar

---
+++ AppAPL.Core/AppAPL.Api/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace AppAPL.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController (ILogger<TestController> logger): ControllerBase
+    {
+        [HttpGet("getstatus")]
+        public async Task<ActionResult> getStatus()
+        {
+            var info = new
+            {
+                Status = "Healthy",
+                Timestamp = DateTime.UtcNow,
+                Server = Environment.MachineName,
+                OS = RuntimeInformation.OSDescription,
+                Runtime = RuntimeInformation.FrameworkDescription
+            };
+
+            return Ok(info);
+        }
+
+        [HttpGet("ping")]
+        public async Task<ActionResult> Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
