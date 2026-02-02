40c6a12686e79f551d1b9456c3b727275f2ec671
joseDG <josethp1@gmail.com>
2026-02-02 16:43:45 -0500

    actualizando tabla de promocionsegmento

--- Oracle/Scripts_Tablas/Apl_Tb_PromocionSegmento.sql
+++ Oracle/Scripts_Tablas/Apl_Tb_PromocionSegmento.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Apl_Tb_PromocionSegmento (
     IdPromocionSegmento     NUMBER(18,0) GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1 NOCACHE ORDER) NOT NULL,
     IdPromocion             NUMBER(18,0) NOT NULL,
-    IdTipoSegmento          VARCHAR2(10) NOT NULL,
+    IdTipoSegmento          NUMBER(18,0) NOT NULL,
 	TipoAsignacion          CHAR(1)      NOT NULL,
     EstadoRegistro          NUMBER(10)   NOT NULL,
     CONSTRAINT PK_Apl_Tb_PromocionSegmento 
